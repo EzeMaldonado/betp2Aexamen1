f0ffcf2718c10a6b6e856d2213938efc5748f131
pablofernandezhinojosa <pablof@tecnoshare.com>
2020-11-02 00:23:24 -0300

    examen yatp2 1

---
+++ pregunta1.js
@@ -0,0 +1,8 @@
+/**
+ * Dato el siguiente array `[3,62,234,7,23,74,23,76,92]` use el metodo filter y una function arrow para:
+ * crear un array que contenga solo los numeros pares
+ */
+
+ const numbers = [3, 62, 234, 7, 23, 74, 23, 76, 92];
+
+ 
